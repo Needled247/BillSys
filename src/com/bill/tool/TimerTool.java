942d89d75875261c008e6abf38358121fd29ef7d
Hao <publicvoidjh@gmail.com>
2013-09-10 17:18:00 +0800

    2013-09-10

--- src/com/bill/tool/TimerTool.java
+++ src/com/bill/tool/TimerTool.java
@@ -1,13 +1,36 @@
 package com.bill.tool;
 
+import org.quartz.Scheduler;
+import org.quartz.SchedulerFactory;
+import org.quartz.impl.StdSchedulerFactory;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
 /**
- * Created with IntelliJ IDEA.
- * User: HP
+ * 定时任务
+ * Author:蒋浩
  * Date: 13-9-10
  * Time: 下午2:07
- * To change this template use File | Settings | File Templates.
  */
-public class TimerTool extends HttpServlet {
+public class TimerTool  extends HttpServlet{
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("Timer Scheduler Start!");
+
+    }
+
+    /**
+     * Quartz实现定时执行任务
+     */
+    protected void initScheduler(ServletConfig config){
+        try{
+            SchedulerFactory sf = new StdSchedulerFactory();
+            Scheduler scheduler = sf.getScheduler();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
