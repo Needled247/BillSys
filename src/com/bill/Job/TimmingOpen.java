ccfd481275369eb306e80f32391d80744ae02777
Hao <publicvoidjh@gmail.com>
2013-09-13 17:36:39 +0800

    2013-09-13

---
+++ src/com/bill/Job/TimmingOpen.java
@@ -0,0 +1,11 @@
+package com.bill.Job;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: HP
+ * Date: 13-9-13
+ * Time: обнГ5:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class TimmingOpen {
+}
---
+++ src/com/bill/tool/QuartzManager.java
@@ -0,0 +1,11 @@
+package com.bill.tool;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: HP
+ * Date: 13-9-13
+ * Time: ионГ9:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class QuartzManager {
+}
